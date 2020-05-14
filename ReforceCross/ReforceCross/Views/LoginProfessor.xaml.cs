083869603c6c0ca376ddb370493cd96004cefb9d
Chico <you@example.com>
2020-05-14 19:32:41 -0300

    adicionando arquivos do projeto ReforceCross

---
+++ ReforceCross/ReforceCross/Views/LoginProfessor.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ReforceCross.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginProfessor : ContentPage
+    {
+        public LoginProfessor()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CadastroProfessor());
+        }
+    }
+};
+
