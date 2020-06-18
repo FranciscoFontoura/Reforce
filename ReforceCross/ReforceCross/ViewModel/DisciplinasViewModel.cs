94ded4fd60611e4865b5fb6a616ed5cdaac77c59
Felipe Braga <felipevbraga@outlook.com>
2020-06-18 19:19:18 -0300

    Controllers de conexão e cadastro, models  view models de disciplinas e  formulário de cadastro.

---
+++ ReforceCross/ReforceCross/ViewModel/DisciplinasViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using ReforceCross.Controllers;
+using ReforceCross.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ReforceCross.ViewModel
+{
+    class DisciplinasViewModel
+    {
+        public ObservableCollection<Disciplinas> Disciplinas { get; set; }
+        public DisciplinasViewModel()
+
+        {
+            Disciplinas = new ObservableCollection<Disciplinas>(LoadList() as List<Disciplinas>);
+        }
+        public List<Disciplinas> LoadList()
+        {
+            List<Disciplinas> disciplinas = new List<Disciplinas>();
+
+            using (IDbConnection connection =
+                new SqlConnection(@"Data Source =DESKTOP-99ATQQ4; Initial Catalog = DBTRABALHO;User Id= sa;Password = 1234"))
+            {
+                disciplinas = connection.Query<Disciplinas>("SELECT * FROM DISCIPLINAS").ToList();
+            }
+            return disciplinas;
+        }
+    }
+}
