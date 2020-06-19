6a8ccb21972f75cc6f27661d24e3dcacb31052dd
Felipe Braga <felipevbraga@outlook.com>
2020-06-18 21:11:49 -0300

    Cadastro new

--- ReforceCross/ReforceCross/Controllers/Cadastro.cs
+++ ReforceCross/ReforceCross/Controllers/Cadastro.cs
@@ -13,32 +13,35 @@ namespace ReforceCross.Controllers
         ConexaoSql con = new ConexaoSql();
 
         public void Cadastrar(DateTime dtcadastro, string nome, string sobrenome, string cep, string rua, string bairro,
-            string cidade, string uf, int numero, string complemento, string mail, string fixo, string celular, /*char disp,*/
+            string cidade, string uf, int numero, string complemento, string mail, string fixo, string celular, char disp,
             SqlMoney valor, string loginuser, string senha, char tipousuario)
         {
 
             Sql = " insert into PROFESSOR (DTCADASTRO, NOME, SOBRENOME, CEP, RUA, BAIRRO, CIDADE,UF, NUMERO, COMPLEMENTO, FIXO, CELULAR, MAIL, DISPONIBILIDADE, VALORAULA) values(@dtcadastro, @nome, @sobrenome, @cep, @rua, @bairro, @cidade, @uf, @numero, @complemento, @fixo, @celular, @mail, @disp, @valor)";
             Sql += " insert into USUARIO(LOGINUSER,SENHA, TIPOUSUARIO) VALUES(@loginuser, @senha, @tipousuario)";
             Command = new SqlCommand(Sql);
+
             Command.Parameters.AddWithValue("@dtcadastro", dtcadastro);
             Command.Parameters.AddWithValue("@nome", nome);
             Command.Parameters.AddWithValue("@sobrenome", sobrenome);
             Command.Parameters.AddWithValue("@cep", cep);
             Command.Parameters.AddWithValue("@rua", rua);
+            Command.Parameters.AddWithValue("@bairro", bairro);
             Command.Parameters.AddWithValue("@cidade", cidade);
-            Command.Parameters.AddWithValue("@uf", cidade);
+            Command.Parameters.AddWithValue("@uf", uf);
             Command.Parameters.AddWithValue("@dtcadastro", dtcadastro);
             Command.Parameters.AddWithValue("@numero", numero);
             Command.Parameters.AddWithValue("@complemento", complemento);
             Command.Parameters.AddWithValue("@mail", mail);
             Command.Parameters.AddWithValue("@fixo", fixo);
             Command.Parameters.AddWithValue("@celular", celular);
-            // Command.Parameters.AddWithValue("@disp", disp);
+            Command.Parameters.AddWithValue("@disp", disp);
             Command.Parameters.AddWithValue("@valor", valor);
             Command.Parameters.AddWithValue("@loginuser", loginuser);
             Command.Parameters.AddWithValue("@senha", senha);
             Command.Parameters.AddWithValue("@tipousuario", tipousuario);
             con.ExercutarComandoSQL(Command);
+
         }
     }
 }
--- ReforceCross/ReforceCross/Views/CadastroProfessor.xaml.cs
+++ ReforceCross/ReforceCross/Views/CadastroProfessor.xaml.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms.Internals;
+using ReforceCross.Controllers;
+using ReforceCross.ViewModel;
+using ReforceCross.Models;
 
 namespace ReforceCross.Views
 {
@@ -17,37 +21,53 @@ namespace ReforceCross.Views
         {
             InitializeComponent();
             profDisciplina.Title = "Selecione uma Disciplina";
-            var disciplinas = new List<string> { "Matematica","História","Portugues","Física"};
-            foreach (String disciplina in disciplinas)
+            List<Disciplinas> disciplinas = new DisciplinasViewModel().LoadList();
+
+            foreach (Disciplinas disciplina in disciplinas)
             {
-                profDisciplina.Items.Add(disciplina);
+                profDisciplina.Items.Add(disciplina.NOME);
             }
+            profDisponibilidade.Title = "Selecione sua Disponibilidade!";
+            var disponibilide = new List<string> { "A", "O", "P" };
+            foreach (String disp in disponibilide)
+            {
+                profDisponibilidade.Items.Add(disp);
+            }
+
         }
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
             if (profSenha.Text == profSenhaConfirm.Text)
             {
-                SqlConnection conn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=ReforceDB;User Id=sa;Password=123456;");
-
-                string SQL = "INSERT INTO Professor (nome, disciplina, bairro, usuario, senha) values (@nome, @disciplina, @bairro, @usuario, @senha)";
-
-                SqlCommand c = new SqlCommand(SQL, conn);
-
-                c.Parameters.AddWithValue("@nome", profNome.Text);
-                c.Parameters.AddWithValue("@disciplina", profDisciplina.SelectedIndex);
-                c.Parameters.AddWithValue("@bairro", profBairro.Text);
-                c.Parameters.AddWithValue("@usuario", profUsuario.Text);
-                c.Parameters.AddWithValue("@senha", profSenha.Text);
-
-                conn.Open();
-                c.ExecuteNonQuery();
-                conn.Close();
-                
+                Cadastro cad = new Cadastro();
+                cad.Cadastrar(
+                    DateTime.Now,
+                    profNome.Text,
+                    profSobrenome.Text,
+                    profCep.Text,
+                    profRua.Text,
+                    profBairro.Text,
+                    profCidade.Text,
+                    profUf.Text,
+                    int.Parse(profNumero.Text),
+                    profComplemento.Text,
+                    profFixo.Text,
+                    profCel.Text,
+                    profMail.Text,
+                    (char)profDisciplina.SelectedItem,
+                    SqlMoney.Parse(profValor.Text),
+                    profUsuario.Text,
+                    profSenha.Text,
+                    'P'
+                    );
+                /*public void Cadastrar(DateTime dtcadastro, string nome, string sobrenome, string cep, string rua, string bairro, 
+            string cidade,string uf, int numero, string complemento, string fixo, string celular, string mail, char disp, 
+            SqlMoney valor, string loginuser, string senha, char tipousuario)*/
             }
             else
             {
-                DisplayAlert("As duas senhas são diferentes!","favor colocar senhas iguais","OK!");
+                DisplayAlert("As duas senhas são diferentes!", "favor colocar senhas iguais", "OK!");
             }
         }
     }
